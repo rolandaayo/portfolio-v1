d3b238a5eaaea282795918ae95cad37e296d90c2
Roland Ibiwoye <contact@rolandayo.com>
2025-01-13 09:32:43 +0100

    fix

---
+++ constants/projectData.ts
@@ -0,0 +1,44 @@
+export const projectData = [
+    {
+        title: "Appvarsity",
+        description:
+            "A platform dedicated to bridging the gap in information and resources for Aptech students by providing them with essential tools and support tailored to their needs. ",
+        technologies: ["React","Next.js", "MongoDB", "Tailwind", "Express.js", "Typescript"],
+        livePreview: "https://appvarsity.vercel.app/",
+        caseStudy: "#",
+        sourceCode: "https://github.com/somkenemuscle/AppVarsity",
+        image: "/assets/images/ap.jpg",
+    },
+    {
+        title: "Retweet",
+        description:
+            "A fun and interactive social media platform designed for sharing posts and connecting with friends, built specifically for my school or any community and friends to create our very own online hangout space! 😂",
+        technologies: ["React","Tailwind", "Next.js", "Typescript", "Express.js", "MongoDb"],
+        livePreview: "https://retweet-frontend.vercel.app/",
+        caseStudy: "#",
+        sourceCode: "https://github.com/somkenemuscle/Retweet-backend",
+        image: "/assets/images/retweet.jpg",
+    },
+    {
+        title: "SwiftDocs",
+        description:
+            "A live document collaboration application that allows users worldwide to share a common space and exchange innovative ideas seamlessly.",
+        technologies: ["React","Tailwind", "MongoDb", "Express.js", "Typescript"],
+        livePreview: "https://swiftdocs.vercel.app/",
+        caseStudy: "#",
+        sourceCode: "https://github.com/somkenemuscle/Swift-Docs",
+        image: "/assets/images/sd.jpg",
+    },
+
+    {
+        title: "Decorvista",
+        description:
+            "An e-commerce platform that allows users to shop for home design appliances and connect with interior designers, creating a collaborative space to showcase and explore innovative designs.",
+        technologies: ["React", "Bootstrap", "Javascript", "Express.js", "MongoDb"],
+        livePreview: "https://decorvista.vercel.app/",
+        caseStudy: "#",
+        sourceCode: "https://github.com/somkenemuscle/decorvista",
+        image: "/assets/images/decor.jpg",
+    },
+
+];
