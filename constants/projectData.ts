98bd7529c56cb8520d84d43155e1a0d70fef017d
Roland Ibiwoye <contact@rolandayo.com>
2025-01-23 21:42:41 +0100

    data tab - 2

--- constants/projectData.ts
+++ constants/projectData.ts
@@ -3,7 +3,7 @@ export const projectData = [
         title: "Clothing Store",
         description:
             "A sleek and responsive clothing store built with Next.js and styled with Tailwind CSS. This app offers a seamless shopping experience, allowing users to browse, add items to their cart, and securely checkout using Paystack integration. Designed to highlight functionality and performance, this project showcases expertise in eCommerce development with a modern tech stack. ",
-        technologies: ["React","Next.js", "MongoDB", "Tailwind", "Express.js", "Typescript"],
+        technologies: ["Next.js", "MongoDB", "Tailwind", "Express.js", "Typescript"],
         livePreview: "https://ecommerce-w3b.vercel.app/",
         caseStudy: "#",
         sourceCode: "https://github.com/rolandaayo/ecommerce-store",
@@ -13,7 +13,7 @@ export const projectData = [
         title: "Foodie",
         description:
             "Foodie is a modern and responsive food ordering app built with Next.js and styled with Tailwind CSS. The app allows users to explore a variety of meals, add items to their cart, and securely complete their orders using Paystack integration. Designed for performance and user experience, Foodie demonstrates expertise in building functional eCommerce solutions for the food industry.",
-        technologies: ["React","Tailwind", "Next.js", "Typescript", "Express.js", "MongoDb"],
+        technologies: ["Next.js","TailwindCSS", "Javascript", "Express.js", "MongoDb"],
         livePreview: "https://foodie-w3b.vercel.app/",
         caseStudy: "#",
         sourceCode: "https://github.com/rolandaayo/foodie",
@@ -22,8 +22,8 @@ export const projectData = [
     {
         title: "Car Rental",
         description:
-            "A live document collaboration application that allows users worldwide to share a common space and exchange innovative ideas seamlessly.",
-        technologies: ["React","Tailwind", "MongoDb", "Express.js", "Typescript"],
+            "CarRent is a dynamic and user-friendly car rental application built with Next.js and styled with Tailwind CSS. The app allows users to browse available vehicles, view detailed information, and book their preferred cars with ease. Designed for speed, responsiveness, and seamless navigation, CarRent highlights expertise in building modern web solutions for the automotive rental industry.",
+        technologies: ["Next.js","TailwindCSS", "MongoDb", "Express.js", "Javascript"],
         livePreview: "https://car-rental-w3b.vercel.app/",
         caseStudy: "#",
         sourceCode: "https://github.com/rolandaayo/car-rental",
@@ -33,8 +33,8 @@ export const projectData = [
     {
         title: "Leaders Network Limited",
         description:
-            "An e-commerce platform that allows users to shop for home design appliances and connect with interior designers, creating a collaborative space to showcase and explore innovative designs.",
-        technologies: ["React", "Bootstrap", "Javascript", "Express.js", "MongoDb"],
+            "A complete redesign and development of the Leaders Network Limited website for an IT company. Built with Next.js, the new site features a modern, responsive design with optimized performance and user experience. From concept to deployment, the project showcases expertise in crafting professional websites tailored to client needs, highlighting the company's services, portfolio, and values effectively.",
+        technologies: ["Next.js", "TailwindCSS", "Javascript", "Express.js", "MongoDb"],
         livePreview: "https://eleadersnetwork.com/",
         caseStudy: "#",
         sourceCode: "https://github.com/rolandaayo/leaders-network-web",
